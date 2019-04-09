84dfb02f6cf597f868a09651d3f01ab3b3554cd9
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-04-09 15:40:05 +0000

    loops

---
+++ loops.js
@@ -0,0 +1,21 @@
+x++ or ++x // same as x = x + 1 
+x-- or --x // same as x = x - 1
+x += 3 // same as x = x + 3
+x -= 6 // same as x = x - 6
+x *= 2 // same as x = x * 2
+x /= 5 // same as x = x / 5
+
+/*
+ * Programming Quiz: Fix the Error 1 (4-5)
+ */
+
+// fix the for loop
+for (var x = 5; x < 10; x++) {
+    console.log(x);
+}
+
+for ( var x = 9 ; x >= 1; x--) {
+  console.log("hello " + x);
+  
+}
+
