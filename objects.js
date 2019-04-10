9ca15d1f0db367e44a75f2a73cd3ca37e36ef601
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-04-10 05:50:00 +0000

    object algorithms facebook added

--- objects.js
+++ objects.js
@@ -30,3 +30,36 @@ var savingsAccount = {
     }
   }
 };
+
+// Create an object called facebookProfile. The object should have 3 properties:
+
+// your name
+// the number of friends you have, and
+// an array of messages you've posted (as strings)
+// The object should also have 4 methods:
+
+// postMessage(message) - adds a new message string to the array
+// deleteMessage(index) - removes the message corresponding to the index provided
+// addFriend() - increases the friend count by 1
+// removeFriend() - decreases the friend count by 
+// your code goes here
+var facebookProfile = {
+  name: "Xavier Carty",
+  friends: 927,
+  messages: ["In Buenos Aires!", "In Stockholm!", "In New York!"],
+  postMessage(message) {
+    facebookProfile.messages.push(message); 
+  }, 
+  deleteMessage(index) {
+    facebookProfile.messages.splice(index,1);  
+  }, 
+  addFriend() {
+    facebookProfile.friends += 1; 
+  },
+  removeFriend() {
+   facebookProfile.friends = facebookProfile.friends -1; 
+  }, 
+}; 
+
+facebookProfile.removeFriend();
+facebookProfile.friends;
