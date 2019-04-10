745e5866edfce092378cd0806e97a46f4112cd5f
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-04-10 05:17:36 +0000

    object literal notation

---
+++ objects.js
@@ -0,0 +1,12 @@
+// The "key" (representing a property or method name) and its "value" are separated from each other by a colon
+// The key: value pairs are separated from each other by commas
+// The entire object is wrapped inside curly braces { }.
+
+var sister = {
+  name: "Sarah", 
+  age: 23,
+  parents: [ "alice", "andy" ],
+  siblings: ["julia"],
+  favoriteColor: "purple",
+  pets: true
+};
