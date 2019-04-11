28ec1d69b1c56f06a07c29df6c9d8bfeaeabae74
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-04-11 07:24:36 +0000

    forofloops

---
+++ forofloop.js
@@ -0,0 +1,13 @@
+let digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+for (const digit of digits) {
+  console.log(digit);
+}
+
+ 
+ digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ,10];
+
+for (const digit of digits) {
+  if (digit % 2 == 0)
+  console.log(digit);
+}
