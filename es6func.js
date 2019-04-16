0a7c3f34c3b93124252217066c99830e4a790b1a
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-04-16 18:14:21 +0000

    generate a grid with default es6 functions

--- es6func.js
+++ es6func.js
@@ -73,3 +73,24 @@ IceCream.prototype.addScoop = function() {
 
 const dessert = new IceCream();
 dessert.addScoop();
+
+//  ES6 has introduced a new way to create defaults. It's called default function parameters
+
+function greet(name = 'Student', greeting = 'Welcome') {
+  return `${greeting} ${name}!`;
+}
+
+greet(); // Welcome Student!
+greet('James'); // Welcome James!
+greet('Richard', 'Howdy'); // Howdy Richard!
+
+// 
+
+function createGrid([width = 5, height = 5]) {
+  return `Generates a ${width} x ${height} grid`;
+}
+
+createGrid([]); // Generates a 5 x 5 grid
+createGrid([2]); // Generates a 2 x 5 grid
+createGrid([2, 3]); // Generates a 2 x 3 grid
+createGrid([undefined, 3]); // Generates a 5 x 3 grid
